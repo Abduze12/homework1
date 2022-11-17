e003937f8d4ab5269f3be5cd289d2d2ede592c4f
Abduze12 <abdu.jasarevic99@gmail.com>
2022-11-17 19:32:33 +0100

    Zadaca1

---
+++ index.js
@@ -0,0 +1,12 @@
+let osoba = {
+    ime: "Abdurahman",
+    prezime: "Jasarevic",
+    godine: "23",
+    grad: "ZENICA",
+     };
+ 
+     console.log(`Ime: ${osoba.ime},
+             Prezime: ${osoba.prezime},
+              Godine: ${osoba.godine},
+              Grad: ${osoba.grad}`);
+ 
